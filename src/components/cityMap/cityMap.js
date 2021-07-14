51c53b3b62cc1b9ecaa61bb9fd98d6a839b6c14e
jarauta1 <diegojarauta8@gmail.com>
2021-07-14 13:06:33 +0200

    Changed temperature label on map

--- src/components/cityMap/cityMap.js
+++ src/components/cityMap/cityMap.js
@@ -3,7 +3,16 @@ import GoogleMapReact from "google-map-react";
 
 import "./cityMap.css";
 
-const Map = ({ text }) => <div><label className="label-map">{text}</label></div>;
+const Map = ({ text }) => 
+  <div class="main-wrapper">
+    <div class="signboard-wrapper">
+      <div class="signboard">{text}</div>
+      <div class="string"></div>
+      <div class="pin pin1"></div>
+      <div class="pin pin2"></div>
+      <div class="pin pin3"></div>
+    </div>
+  </div>;
 
 class CityMap extends Component {
    
@@ -11,7 +20,7 @@ class CityMap extends Component {
     return (
       <div style={{ height: '100%', width: '100%' }}>
         <GoogleMapReact center={{lat: this.props.lat, lng: this.props.long}} defaultZoom={7}>
-          <Map center={{lat: this.props.lat, lng: this.props.long}} zoom={7} lat={this.props.lat} lng={this.props.long} text={"Temp:"+this.props.temp+"ºC"}/>
+          <Map center={{lat: this.props.lat, lng: this.props.long}} zoom={7} lat={this.props.lat} lng={this.props.long} text={this.props.temp+"ºC"}/>
         </GoogleMapReact>
       </div>
     );
--- src/components/generalMap/generalMap.js
+++ src/components/generalMap/generalMap.js
@@ -3,7 +3,16 @@ import GoogleMapReact from "google-map-react";
 
 import "../cityMap/cityMap.css";
 
-const Map = ({ text }) => <div><label className="label-map">{text}</label></div>;
+const Map = ({ text }) => 
+  <div class="main-wrapper">
+    <div class="signboard-wrapper">
+      <div class="signboard">{text}</div>
+      <div class="string"></div>
+      <div class="pin pin1"></div>
+      <div class="pin pin2"></div>
+      <div class="pin pin3"></div>
+    </div>
+  </div>;
 
 class GeneralMap extends Component {
   static defaultProps = {
@@ -18,14 +27,14 @@ class GeneralMap extends Component {
     return (
       <div style={{ height: '100%', width: '100%' }}>
         <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
-          <Map lat={this.props.cityInfo.almLat} lng={this.props.cityInfo.almLon} text={`Temp:${(this.props.cityInfo.almTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.cadLat} lng={this.props.cityInfo.cadLon} text={`Temp:${(this.props.cityInfo.cadTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.corLat} lng={this.props.cityInfo.corLon} text={`Temp:${(this.props.cityInfo.corTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.graLat} lng={this.props.cityInfo.graLon} text={`Temp:${(this.props.cityInfo.graTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.hueLat} lng={this.props.cityInfo.hueLon} text={`Temp:${(this.props.cityInfo.hueTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.jaeLat} lng={this.props.cityInfo.jaeLon} text={`Temp:${(this.props.cityInfo.jaeTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.malLat} lng={this.props.cityInfo.malLon} text={`Temp:${(this.props.cityInfo.malTemp-273.15).toFixed(1)}ºC`}/>
-          <Map lat={this.props.cityInfo.sevLat} lng={this.props.cityInfo.sevLon} text={`Temp:${(this.props.cityInfo.sevTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.almLat} lng={this.props.cityInfo.almLon} text={`${(this.props.cityInfo.almTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.cadLat} lng={this.props.cityInfo.cadLon} text={`${(this.props.cityInfo.cadTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.corLat} lng={this.props.cityInfo.corLon} text={`${(this.props.cityInfo.corTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.graLat} lng={this.props.cityInfo.graLon} text={`${(this.props.cityInfo.graTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.hueLat} lng={this.props.cityInfo.hueLon} text={`${(this.props.cityInfo.hueTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.jaeLat} lng={this.props.cityInfo.jaeLon} text={`${(this.props.cityInfo.jaeTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.malLat} lng={this.props.cityInfo.malLon} text={`${(this.props.cityInfo.malTemp-273.15).toFixed(1)}ºC`}/>
+          <Map lat={this.props.cityInfo.sevLat} lng={this.props.cityInfo.sevLon} text={`${(this.props.cityInfo.sevTemp-273.15).toFixed(1)}ºC`}/>
         </GoogleMapReact>
       </div>
     );
